45e724818ef3b0fe1ce59b8f2b583e8b788ef232
Michael Mack <michael.d.c.mack@gmail.com>
2015-12-15 16:54:48 -0800

    Fix up some Unity 5+ warnings in PlayMaker.

--- RL001/Assets/PlayMaker/Actions/RestartLevel.cs
+++ RL001/Assets/PlayMaker/Actions/RestartLevel.cs
@@ -1,18 +1,18 @@
 // (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
 // micro script by Andrew Raphael Lukasik
 
-using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace HutongGames.PlayMaker.Actions
 {
-	[ActionCategory(ActionCategory.Level)]
-	[Tooltip("Restarts current level.")]
-	public class RestartLevel : FsmStateAction
-	{
-	    public override void OnEnter()
-	    {
-		    Application.LoadLevel(Application.loadedLevelName);
-	        Finish();
-	    }
-	}
+    [ActionCategory(ActionCategory.Level)]
+    [Tooltip("Restarts current level.")]
+    public class RestartLevel : FsmStateAction
+    {
+        public override void OnEnter()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Finish();
+        }
+    }
 }
